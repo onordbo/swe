8998b7569afee63557415f452ae4ca18b44839a7
Øyvind Nordbø <oyvinor@gmail.com>
2015-08-12 12:01:57 +0200

    Add Sweet-alert

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var framerDisclaimer = function () {
+	swal({
+	  title: "Ahoy!",
+	  text: "This prototype is best viewed on an iPhone 6 with or a desktop web browser.",
+	  showCancelButton: true,
+	  confirmButtonText: "Show me the stuff." });
+}
